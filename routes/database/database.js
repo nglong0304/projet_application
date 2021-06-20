2a0347b13914736f4324252ea831505d659b1ca4
Long <nglong0304@gmail.com>
2021-06-20 16:55:16 +0000

    display all module into the page_accueil

---
+++ routes/database/database.js
@@ -0,0 +1,31 @@
+const mariadb = require('mariadb');
+
+const pool = mariadb.createPool({
+    host: 'localhost',
+    user: 'long',
+    password: 'long7412',
+    database: 'database_application',
+    connectionLimit: 5
+});
+
+
+
+// Connect and check for errors
+pool.getConnection((err, connection) => {
+    if (err) {
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            console.error('Database connection lost');
+        }
+        if (err.code === 'ER_CON_COUNT_ERROR') {
+            console.error('Database has too many connection');
+        }
+        if (err.code === 'ECONNREFUSED') {
+            console.error('Database connection was refused');
+        }
+    }
+    if (connection) connection.release();
+
+    return;
+});
+
+module.exports = pool;
--- routes/web/home.js
+++ routes/web/home.js
@@ -2,11 +2,25 @@
 var express = require("express");
 
 var router = express.Router();
+const pool = require('../database/database.js');
 
 
+router.get("/", async function(req, res) {
+    var ID_MODULES = await pool.query('SELECT ID_MODULES FROM MODULE')
+    var ID_USER = await pool.query('SELECT ID_USER FROM MODULE')
+    var NAME_MODULE = await pool.query('SELECT NAME_MODULE FROM MODULE')
+    var DESCRIPTION = await pool.query('SELECT DESCRIPTION FROM MODULE')
+    var CLE = await pool.query('SELECT CLE FROM MODULE')
+    var MOYEN_NOTE = await pool.query('SELECT MOYEN_NOTE FROM MODULE')
 
-router.get("/", function(req, res) {
-    res.render("home/index");
+    res.render("home/index", {
+        ID_MODULES: ID_MODULES,
+        ID_USER: ID_USER,
+        NAME_MODULE: NAME_MODULE,
+        DESCRIPTION: DESCRIPTION,
+        CLE: CLE,
+        MOYEN_NOTE: MOYEN_NOTE
+    });
 });
 
 router.get("/home", function(req, res) {
