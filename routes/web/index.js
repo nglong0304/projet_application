a7122dfeea3f51805587e78123edc1c7fae52bb0
chungngoc <tongngocchungbtqb@gmail.com>
2021-06-25 06:27:42 -0400

    delete console

--- routes/web/index.js
+++ routes/web/index.js
@@ -2,13 +2,6 @@ var express = require("express");
 
 var router = express.Router();
 
-router.use(function(req, res, next) {
-    res.locals.currentUser = req.query.user;
-    res.locals.currentId = req.query.id;
-    next();
-    // console.log(res.locals.currentId);
-});
-
 router.use("/", require("./home"));
 
 
